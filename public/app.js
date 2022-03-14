7721b3d429850b056415ec4d913ae15d6ab104a5
namdhgc <namdhgc@gmail.com>
2022-03-14 14:54:51 +0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", (_) => {
+  // let websocket = new WebSocket("ws://" + window.location.host + "/websocket");
+  const host = "localhost:4444";
+  let websocket = new WebSocket("ws://" + host + "/websocket");
+
+  let room = document.getElementById("chat-text");
+
+  websocket.addEventListener("message", function (e) {
+    let data = JSON.parse(e.data);
+    // creating html element
+    let p = document.createElement("p");
+    p.innerHTML = `<strong>${data.username}</strong>: ${data.text}`;
+
+    room.append(p);
+    room.scrollTop = room.scrollHeight; // Auto scroll to the bottom
+  });
+
+  let form = document.getElementById("input-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("input-username");
+    let text = document.getElementById("input-text");
+    websocket.send(
+      JSON.stringify({
+        username: username.value,
+        text: text.value,
+      })
+    );
+    text.value = "";
+  });
+});
